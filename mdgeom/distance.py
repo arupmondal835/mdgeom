737c41aa495ab23bd4810af4810084c619cc4aee
arup mondal <arup.mondal@ufl.edu>
2024-06-24 17:44:24 -0700

    added code

---
+++ mdgeom/distance.py
@@ -0,0 +1,7 @@
+"""
+This is a heading.
+This is a gile where I write code for calculating distances.
+
+
+"""
+import numpy as np
---
+++ mdgeom/info.py
@@ -0,0 +1,13 @@
+"""
+Information about Simulations
+==============================
+
+This modules contains function that provie overview information.
+"""
+
+def summary(*universe):
+    pass
+
+def extract(universe):
+    pass
+
